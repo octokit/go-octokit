7a5f1d339f3a7ef451a10a6ef25aa683a6f2c0fd
Jingwen Owen Ou <jingweno@gmail.com>
2013-09-28 12:50:57 -0700

    Use pull_request.json fixture for tests

--- pull_requests.go
+++ pull_requests.go
@@ -5,82 +5,76 @@ import (
 	"time"
 )
 
-type PullRequestParams struct {
-	Base  string `json:"base"`
-	Head  string `json:"head"`
-	Title string `json:"title"`
-	Body  string `json:"body"`
-}
-
-type PullRequestForIssueParams struct {
-	Base  string `json:"base"`
-	Head  string `json:"head"`
-	Issue string `json:"issue"`
-}
-
 type PullRequest struct {
-	URL               string     `json:"url"`
-	ID                int        `json:"id"`
-	HTMLURL           string     `json:"html_url"`
-	DiffURL           string     `json:"diff_url"`
-	PatchURL          string     `json:"patch_url"`
-	IssueURL          string     `json:"issue_url"`
-	Number            int        `json:"number"`
-	State             string     `json:"state"`
-	Title             string     `json:"title"`
-	User              User       `json:"user"`
-	Body              string     `json:"body"`
-	CreatedAt         time.Time  `json:"created_at"`
-	UpdatedAt         time.Time  `json:"updated_at"`
-	ClosedAt          *time.Time `json:"closed_at"`
-	MergedAt          *time.Time `json:"merged_at"`
-	MergedCommitSha   string     `json:"merged_commit_sha"`
-	Assignee          *User      `json:"assignee"`
-	CommitsUrl        string     `json:"commits_url"`
-	ReviewCommentsUrl string     `json:"review_comments_url"`
-	ReviewCommentUrl  string     `json:"review_comment_url"`
-	CommentsUrl       string     `json:"comments_url"`
-	Head              Commit     `json:"head"`
-	Base              Commit     `json:"base"`
-	Merged            bool       `json:"merged"`
-	MergedBy          User       `json:"merged_by"`
-	Comments          int        `json:"comments"`
-	ReviewComments    int        `json:"review_comments"`
-	Commits           int        `json:"commits"`
-	Additions         int        `json:"additions"`
-	Deletions         int        `json:"deletions"`
-	ChangedFiles      int        `json:"changed_files"`
+	URL               string     `json:"url,omitempty"`
+	ID                int        `json:"id,omitempty"`
+	HTMLURL           string     `json:"html_url,omitempty"`
+	DiffURL           string     `json:"diff_url,omitempty"`
+	PatchURL          string     `json:"patch_url,omitempty"`
+	IssueURL          string     `json:"issue_url,omitempty"`
+	Number            int        `json:"number,omitempty"`
+	State             string     `json:"state,omitempty"`
+	Title             string     `json:"title,omitempty"`
+	User              User       `json:"user,omitempty"`
+	Body              string     `json:"body,omitempty"`
+	CreatedAt         time.Time  `json:"created_at,omitempty"`
+	UpdatedAt         time.Time  `json:"updated_at,omitempty"`
+	ClosedAt          *time.Time `json:"closed_at,omitempty"`
+	MergedAt          *time.Time `json:"merged_at,omitempty"`
+	MergeCommitSha    string     `json:"merge_commit_sha,omitempty"`
+	Assignee          *User      `json:"assignee,omitempty"`
+	CommitsURL        string     `json:"commits_url,omitempty"`
+	ReviewCommentsURL string     `json:"review_comments_url,omitempty"`
+	ReviewCommentURL  string     `json:"review_comment_url,omitempty"`
+	CommentsURL       string     `json:"comments_url,omitempty"`
+	Head              Commit     `json:"head,omitempty"`
+	Base              Commit     `json:"base,omitempty"`
+	Merged            bool       `json:"merged,omitempty"`
+	MergedBy          User       `json:"merged_by,omitempty"`
+	Comments          int        `json:"comments,omitempty"`
+	ReviewComments    int        `json:"review_comments,omitempty"`
+	Commits           int        `json:"commits,omitempty"`
+	Additions         int        `json:"additions,omitempty"`
+	Deletions         int        `json:"deletions,omitempty"`
+	ChangedFiles      int        `json:"changed_files,omitempty"`
 }
 
 type Commit struct {
-	Label string     `json:"label"`
-	Ref   string     `json:"ref"`
-	Sha   string     `json:"sha"`
-	User  User       `json:"user"`
-	Repo  Repository `json:"repo"`
+	Label string     `json:"label,omitempty"`
+	Ref   string     `json:"ref,omitempty"`
+	Sha   string     `json:"sha,omitempty"`
+	User  User       `json:"user,omitempty"`
+	Repo  Repository `json:"repo,omitempty"`
 }
 
+// Get a pull request
+//
+// See http://developer.github.com/v3/pulls/#get-a-single-pull-request
 func (c *Client) PullRequest(repo Repo, number string, options *Options) (pr *PullRequest, err error) {
 	path := fmt.Sprintf("repos/%s/pulls/%s", repo, number)
 	err = c.jsonGet(path, options, &pr)
 	return
 }
 
-func (c *Client) CreatePullRequest(repo Repo, options *Options) (*PullRequest, error) {
-	return c.createPullRequest(repo, options)
+type PullRequestParams struct {
+	Base  string `json:"base,omitempty"`
+	Head  string `json:"head,omitempty"`
+	Title string `json:"title,omitempty"`
+	Body  string `json:"body,omitempty"`
 }
 
-func (c *Client) CreatePullRequestForIssue(repo Repo, options *Options) (*PullRequest, error) {
-	return c.createPullRequest(repo, options)
+type PullRequestForIssueParams struct {
+	Base  string `json:"base,omitempty"`
+	Head  string `json:"head,omitempty"`
+	Issue string `json:"issue,omitempty"`
 }
 
-func (c *Client) createPullRequest(repo Repo, options *Options) (*PullRequest, error) {
+// Create a pull request
+//
+// See http://developer.github.com/v3/pulls/#create-a-pull-request
+// See http://developer.github.com/v3/pulls/#alternative-input
+func (c *Client) CreatePullRequest(repo Repo, options *Options) (pr *PullRequest, err error) {
 	path := fmt.Sprintf("repos/%s/pulls", repo)
-	var pr PullRequest
-	err := c.jsonPost(path, options, &pr)
-	if err != nil {
-		return nil, err
-	}
-
-	return &pr, nil
+	err = c.jsonPost(path, options, &pr)
+	return
 }
