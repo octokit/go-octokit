caca0da2cc3542746ba9ba807cb162adc9334bdf
Jingwen Owen Ou <jingweno@gmail.com>
2013-09-28 14:04:44 -0700

    Test users APIs using fixtures

--- users_test.go
+++ users_test.go
@@ -1,23 +1,36 @@
 package octokat
 
 import (
-	//"github.com/bmizerany/assert"
-	//"os"
+	"github.com/bmizerany/assert"
+	"net/http"
 	"testing"
 )
 
 func TestUser(t *testing.T) {
-	//c := NewClient()
-	//user, err := c.User("jingweno")
+	setup()
+	defer tearDown()
 
-	//assert.Equal(t, nil, err)
-	//assert.Equal(t, "jingweno", user.Login)
-}
+	mux.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		respondWith(w, loadFixture("user.json"))
+	})
 
-func TestAuthenticatedUser(t *testing.T) {
-	//c := NewClient().WithToken(os.Getenv("GITHUB_TOKEN"))
-	//user, err := c.User("")
+	user, _ := client.User("")
 
-	//assert.Equal(t, nil, err)
-	//assert.NotEqual(t, "", user.Login)
+	assert.Equal(t, 169064, user.ID)
+	assert.Equal(t, "jingweno", user.Login)
+	assert.Equal(t, "jingweno@gmail.com", user.Email)
+	assert.Equal(t, "User", user.Type)
+
+	mux.HandleFunc("/users/jingweno", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		respondWith(w, loadFixture("user.json"))
+	})
+
+	user, _ = client.User("jingweno")
+
+	assert.Equal(t, 169064, user.ID)
+	assert.Equal(t, "jingweno", user.Login)
+	assert.Equal(t, "jingweno@gmail.com", user.Email)
+	assert.Equal(t, "User", user.Type)
 }
