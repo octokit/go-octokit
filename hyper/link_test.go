e8acd60d882021a2ab42bac350535008e73e648e
Jingwen Owen Ou <jingweno@gmail.com>
2013-10-04 11:24:36 -0700

    Add test case for Link#Expand() with nil or empty M

--- hyper/link_test.go
+++ hyper/link_test.go
@@ -5,8 +5,15 @@ import (
 	"testing"
 )
 
-func TestExpand(t *testing.T) {
+func TestLink_Expand(t *testing.T) {
 	link := Link("https://api.github.com/users/{user}")
 	url, _ := link.Expand(M{"user": "jingweno"})
 	assert.Equal(t, "https://api.github.com/users/jingweno", url)
+
+	link = Link("https://api.github.com/user")
+	url, _ = link.Expand(nil)
+	assert.Equal(t, "https://api.github.com/user", url)
+
+	url, _ = link.Expand(M{})
+	assert.Equal(t, "https://api.github.com/user", url)
 }
