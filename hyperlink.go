f210633395f19738f26a70c2a5851b8229c75abd
Jingwen Owen Ou <jingweno@gmail.com>
2013-10-03 17:15:46 -0700

    Add Hyperlink#Expand

--- hyperlink.go
+++ hyperlink.go
@@ -1,3 +1,23 @@
 package octokat
 
+import (
+	"github.com/jtacoma/uritemplates"
+)
+
+type M map[string]interface{}
+
 type Hyperlink string
+
+func (l *Hyperlink) Expand(m M) (string, error) {
+	template, err := uritemplates.Parse(string(*l))
+	if err != nil {
+		return "", err
+	}
+
+	expanded, err := template.Expand(m)
+	if err != nil {
+		return "", err
+	}
+
+	return expanded, nil
+}
--- hyperlink_test.go
+++ hyperlink_test.go
@@ -1,7 +1,12 @@
 package octokat
 
 import (
-//"github.com/bmizerany/assert"
-//"net/http"
-//"testing"
+	"github.com/bmizerany/assert"
+	"testing"
 )
+
+func TestExpand(t *testing.T) {
+	link := Hyperlink("https://api.github.com/users/{user}")
+	url, _ := link.Expand(M{"user": "jingweno"})
+	assert.Equal(t, "https://api.github.com/users/jingweno", url)
+}
