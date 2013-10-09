e85d533bdfbd75af0f4a45cb39bfad5538c5b7ff
Jingwen Owen Ou <jingweno@gmail.com>
2013-10-09 11:51:34 -0700

    Rename Page to pageable
    pageable is a struct meant to be embedded into Result so that we could call the next page of result like Result.NextPage

--- octokit/result.go
+++ octokit/result.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-type Page struct {
+type pageable struct {
 	NextPage  *url.URL
 	LastPage  *url.URL
 	FirstPage *url.URL
@@ -15,7 +15,7 @@ type Page struct {
 type Result struct {
 	Response *Response
 	Err      error
-	Page
+	pageable
 }
 
 func (r *Result) HasError() bool {
@@ -31,49 +31,50 @@ func (r *Result) Error() string {
 }
 
 func newResult(resp *Response, err error) *Result {
-	var page Page
+	var p pageable
 	if resp != nil {
-		page = parsePage(resp.Header.Get("Link"))
+		p = parsePageable(resp.Header.Get("Link"))
 	}
 
-	return &Result{Response: resp, Page: page, Err: err}
+	return &Result{Response: resp, pageable: p, Err: err}
 }
 
-func parsePage(link string) Page {
-	page := Page{}
+func parsePageable(link string) (p pageable) {
+	p = pageable{}
+	if len(link) == 0 {
+		return
+	}
 
-	if len(link) > 0 {
-		for _, l := range strings.Split(link, ",") {
-			l = strings.TrimSpace(l)
-			segments := strings.Split(l, ";")
+	for _, l := range strings.Split(link, ",") {
+		l = strings.TrimSpace(l)
+		segments := strings.Split(l, ";")
 
-			if len(segments) < 2 {
-				continue
-			}
+		if len(segments) < 2 {
+			continue
+		}
 
-			if !strings.HasPrefix(segments[0], "<") || !strings.HasSuffix(segments[0], ">") {
-				continue
-			}
+		if !strings.HasPrefix(segments[0], "<") || !strings.HasSuffix(segments[0], ">") {
+			continue
+		}
 
-			url, err := url.Parse(segments[0][1 : len(segments[0])-1])
-			if err != nil {
-				continue
-			}
+		url, err := url.Parse(segments[0][1 : len(segments[0])-1])
+		if err != nil {
+			continue
+		}
 
-			for _, segment := range segments[1:] {
-				switch strings.TrimSpace(segment) {
-				case `rel="next"`:
-					page.NextPage = url
-				case `rel="prev"`:
-					page.PrevPage = url
-				case `rel="first"`:
-					page.FirstPage = url
-				case `rel="last"`:
-					page.LastPage = url
-				}
+		for _, segment := range segments[1:] {
+			switch strings.TrimSpace(segment) {
+			case `rel="next"`:
+				p.NextPage = url
+			case `rel="prev"`:
+				p.PrevPage = url
+			case `rel="first"`:
+				p.FirstPage = url
+			case `rel="last"`:
+				p.LastPage = url
 			}
 		}
 	}
 
-	return page
+	return
 }
