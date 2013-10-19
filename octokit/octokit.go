5ba14433c2ce87b0d20f39ad496c8df1176589c8
Jingwen Owen Ou <jingweno@gmail.com>
2013-10-19 11:58:23 -0700

    Fix user agent name

--- octokit/octokit.go
+++ octokit/octokit.go
@@ -7,7 +7,7 @@ import (
 const (
 	GitHubURL          = "https://github.com"
 	GitHubAPIURL       = "https://api.github.com"
-	UserAgent          = "Octokat Go " + Version
+	UserAgent          = "Octokit Go " + Version
 	MediaType          = "application/vnd.github.beta+json"
 	DefaultContentType = "application/json"
 	Version            = "0.3.0"
