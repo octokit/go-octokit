6faaabfb8af0984067be0ac672abbf587138c928
Jingwen Owen Ou <jingweno@gmail.com>
2013-10-20 07:46:29 -0700

    Add more http verbs

--- octokit/http/client_test.go
+++ octokit/http/client_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestGet(t *testing.T) {
+func TestSuccessfulGet(t *testing.T) {
 	setup()
 	defer tearDown()
 
@@ -25,4 +25,28 @@ func TestGet(t *testing.T) {
 	assert.Equal(t, nil, err)
 	assert.T(t, !resp.IsError())
 	assert.Equal(t, "octokit", m["login"])
+}
+
+func TestSuccessfulPost(t *testing.T) {
+	setup()
+	defer tearDown()
+
+	mux.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		testHeader(t, r, "Content-Type", "application/json")
+		testBody(t, r, "{\"input\":\"bar\"}\n")
+		head := w.Header()
+		head.Set("Content-Type", "application/json")
+		respondWith(w, `{"login": "octokit"}`)
+	})
+
+	req, err := client.NewRequest("foo")
+	assert.Equal(t, nil, err)
+
+	input := map[string]interface{}{"input": "bar"}
+	var output map[string]interface{}
+	resp, err := req.Post(input, &output)
+	assert.Equal(t, nil, err)
+	assert.T(t, !resp.IsError())
+	assert.Equal(t, "octokit", output["login"])
 }
--- octokit/http/request.go
+++ octokit/http/request.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"github.com/lostisland/go-sawyer"
+	"github.com/lostisland/go-sawyer/mediatype"
 )
 
 type Headers map[string]string
@@ -12,20 +13,53 @@ type Request struct {
 }
 
 func (r *Request) Head(output interface{}) (resp *Response, err error) {
-	sawyerResp := r.sawyerReq.Head(output)
-	if sawyerResp.IsError() {
-		err = sawyerResp.ResponseError
-		return
-	}
-
-	respErr := NewResponseError(sawyerResp)
-	resp = &Response{Response: sawyerResp.Response, Error: respErr}
-
+	resp, err = r.do("HEAD", nil, output)
 	return
 }
 
 func (r *Request) Get(output interface{}) (resp *Response, err error) {
-	sawyerResp := r.sawyerReq.Get(output)
+	resp, err = r.do("GET", nil, output)
+	return
+}
+
+func (r *Request) Post(input interface{}, output interface{}) (resp *Response, err error) {
+	resp, err = r.do("POST", input, output)
+	return
+}
+
+func (r *Request) Put(input interface{}, output interface{}) (resp *Response, err error) {
+	resp, err = r.do("PUT", input, output)
+	return
+}
+
+func (r *Request) Delete(output interface{}) (resp *Response, err error) {
+	resp, err = r.do("DELETE", nil, output)
+	return
+}
+
+func (r *Request) do(method string, input interface{}, output interface{}) (resp *Response, err error) {
+	var sawyerResp *sawyer.Response
+	switch method {
+	case "HEAD":
+		sawyerResp = r.sawyerReq.Head(output)
+	case "GET":
+		sawyerResp = r.sawyerReq.Get(output)
+	case "POST":
+		mtype, _ := mediatype.Parse("application/json")
+		r.sawyerReq.SetBody(mtype, input)
+		sawyerResp = r.sawyerReq.Post(output)
+	case "PUT":
+		mtype, _ := mediatype.Parse("application/json")
+		r.sawyerReq.SetBody(mtype, input)
+		sawyerResp = r.sawyerReq.Put(output)
+	case "PATCH":
+		sawyerResp = r.sawyerReq.Patch(output)
+	case "DELETE":
+		sawyerResp = r.sawyerReq.Delete(output)
+	case "OPTIONS":
+		sawyerResp = r.sawyerReq.Options(output)
+	}
+
 	if sawyerResp.IsError() {
 		err = sawyerResp.ResponseError
 		return
