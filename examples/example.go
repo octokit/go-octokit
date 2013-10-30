5a3f6e0df41223f4089f016888eb799eaaceb1b5
Jingwen Owen Ou <jingweno@gmail.com>
2013-10-30 13:54:02 -0700

    Add README for the new APIs

---
+++ examples/example.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/octokit/go-octokit"
+)
+
+func main() {
+	client := octokit.NewClient()
+	usersLink := &octokit.AllUsersHyperlink
+
+	fmt.Println("Printing GitHub users for the first 5 pages")
+	for i := 0; i < 5; i++ {
+		if usersLink == nil {
+			return
+		}
+
+		usersService, err := client.Users(usersLink, nil)
+		if err != nil {
+			fmt.Printf("error: %s\n", err)
+			return
+		}
+
+		users, result := usersService.GetAll()
+		if result.HasError() {
+			fmt.Println(result)
+			return
+		}
+
+		for _, user := range users {
+			fmt.Printf("%v - %s\n", user.ID, user.Login)
+		}
+
+		usersLink = result.NextPage
+	}
+}
