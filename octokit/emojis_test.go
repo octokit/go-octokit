0e5b11abb89706e742b1f54c208fa05b5522bc39
Wynn Netherland <wynn@github.com>
2014-01-25 15:50:50 -0600

    Fix failing test

--- octokit/emojis_test.go
+++ octokit/emojis_test.go
@@ -21,7 +21,7 @@ func TestRootEmojisService_All(t *testing.T) {
 	emojis, result := client.Emojis(url).All()
 	assert.T(t, !result.HasError())
 	var penguin = "https://github.global.ssl.fastly.net/images/icons/emoji/penguin.png?v5"
-	var metal = "https://github.global.ssl.fastly.com/images/icons/emoji/metal.png?v5"
+	var metal = "https://github.global.ssl.fastly.net/images/icons/emoji/metal.png?v5"
 	assert.Equal(t, penguin, emojis["penguin"])
 	assert.Equal(t, metal, emojis["metal"])
 }
