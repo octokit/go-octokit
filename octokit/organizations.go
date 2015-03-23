5379b6aa6c18ab28c04aef7ebd788ff199f2c799
lalalah <fc254@cornell.edu>
2015-03-23 00:33:45 -0400

    defining fields

--- octokit/organizations.go
+++ octokit/organizations.go
@@ -11,12 +11,26 @@ var (
 )
 
 type Organization struct {
-	AvatarURL        string `json:"avatar_url,omitempty"`
-	PublicMembersURL string `json:"public_member_url,omitempty"`
-	MembersURL       string `json:"members_url,omitempty"`
-	EventsURL        string `json:"events_url,omitempty"`
-	ReposURL         string `json:"repos_url,omitempty"`
-	URL              string `json:"url,omitempty"`
-	ID               int    `json:"id,omitempty"`
-	Login            string `json:"login,omitempty"`
+	AvatarURL        string     `json:"avatar_url,omitempty"`
+	PublicMembersURL Hyperlink  `json:"public_member_url,omitempty"`
+	MembersURL       Hyperlink  `json:"members_url,omitempty"`
+	EventsURL        Hyperlink  `json:"events_url,omitempty"`
+	ReposURL         Hyperlink  `json:"repos_url,omitempty"`
+	URL              string     `json:"url,omitempty"`
+	ID               int        `json:"id,omitempty"`
+	Login            string     `json:"login,omitempty"`
+	Description      string     `json:"description, omitempty"`
+	Name             string     `json:"name, omitempty"`
+	Company          string     `json:"company, omitempty"`
+	Blog             string     `json:"blog, omitempty"`
+	Location         string     `json:"location, omitempty"`
+	Email            string     `json:"email, omitempty"`
+	PublicRepos      int        `json:"public_repos,omitempty"`
+	PublicGists      int        `json:"public_gists,omitempty"`
+	Followers        int        `json:"followers,omitempty"`
+	Followering      int        `json:"following,omitempty"`
+	HTMLURL          string     `json:"html_url,omitempty"`
+	CreatedAt        *time.Time `json:"created_at,omitempty"`
+	UpdatedAt        *time.Time `json:"updated_at,omitempty"`
+	Type             string     `json:"type,omitempty"`
 }
