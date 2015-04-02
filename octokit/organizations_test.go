5427a51e768c13aca4998c2bdfa962d3ac04585a
feiranchen <fc254@cornell.edu>
2015-04-02 01:03:22 -0400

    added 3rd and 4th test cases

--- octokit/organizations_test.go
+++ octokit/organizations_test.go
@@ -35,6 +35,38 @@ func TestOrganizationService_Info(t *testing.T) {
 	assert.Equal(t, 3430433, organizationResults.ID)
 }
 
+func TestOrganizationService_Yours(t *testing.T) {
+	setup()
+	defer tearDown()
+
+	stubGet(t, "/user/orgs", "organizations", nil)
+
+	organizationResults, result := client.Organization().YourOrganizations(nil, nil)
+
+	assert.False(t, result.HasError())
+	assert.Equal(t, 2, len(organizationResults))
+	assert.Equal(t, 1388703, organizationResults[0].ID)
+	assert.Equal(t, "acl-services", organizationResults[0].Login)
+	assert.Equal(t, 3430433, organizationResults[1].ID)
+	assert.Equal(t, "octokit", organizationResults[1].Login)
+}
+
+func TestOrganizationService_User(t *testing.T) {
+	setup()
+	defer tearDown()
+
+	stubGet(t, "/users/rails/orgs", "organizations", nil)
+
+	organizationResults, result := client.Organization().UserOrganizations(nil, M{"username": "rails"})
+
+	assert.False(t, result.HasError())
+	assert.Equal(t, 2, len(organizationResults))
+	assert.Equal(t, 1388703, organizationResults[0].ID)
+	assert.Equal(t, "acl-services", organizationResults[0].Login)
+	assert.Equal(t, 3430433, organizationResults[1].ID)
+	assert.Equal(t, "octokit", organizationResults[1].Login)
+}
+
 func TestOrganizationService_Failure(t *testing.T) {
 	setup()
 	defer tearDown()
